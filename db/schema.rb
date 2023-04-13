67857b5ec1338baa97b0b9cd8b13fbd47fbc4c85
Kaleshe Alleyne-Vassel <kaleshe.alleyne-vassel@krystal.uk>
2023-04-13 13:04:58 +0100

    generated user model and controller

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_12_105640) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_13_115530) do
   create_table "events", force: :cascade do |t|
     t.string "title"
     t.text "description"
@@ -18,6 +18,13 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_12_105640) do
     t.string "date"
     t.string "time"
     t.string "image"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
